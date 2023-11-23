204fd3c8cbfa63dc09c3d9aa48f241e044a596dd
hplaursen <email@hanspeter.dk>
2023-11-23 23:06:51 +0100

    oprettelse af ordre

--- hpl.java
+++ hpl.java
@@ -1,37 +1,73 @@
 import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 
 public class Hpl {
 
-    static String URL = "jdbc:mysql://localhost:3306/pizza";
-
 
     public static void main(String args[]) {
-/*
- Det er så her jeg ville lave min 
- 
- */
-
+        
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(URL, "root", "");
-
+            Connection connection = connectDB();         
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM `pizzamenu`");
+            /*
+             * Kunde (KundeID 78) bestiller 3 pizzaer: 1 stk. Fire Cheese (5) og 2 stk. Meat Lovers (15)
+             * Pizzaerne bliver afhentet
+             * 
+             * 
+             */
+
+            int[][] pizza = {{5, 1},
+                            {15,2}};
+            LocalDateTime ctime = LocalDateTime.now();
+
+            //opretter ordre: indsætter kundeID etc.
+            String sql = 
+                    "INSERT INTO ordrer(KundeID, Oprettet, Leveres) "+
+                    "VALUES (78, '"+ctime+"', 0)";
+
+            PreparedStatement pstatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstatement.executeUpdate();
+            ResultSet rs = pstatement.getGeneratedKeys();
+            rs.next();
+            int OrdrerID = rs.getInt(1);
+
+            //forbinder ordre med pizzaIDer
+            for (int[] p : pizza) {
+                sql =   "INSERT INTO pizzaordrer(PizzaID, OrdrerID, Antal) VALUES ("+p[0]+", "+OrdrerID+", "+p[1]+")";
+                statement.executeUpdate(sql);
+            };
+
+
+            //viser ordren
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ordrer_view WHERE ID="+OrdrerID);
+            int totalprice = 0;
 
             while (resultSet.next()) {
 
-                System.out.println("Pizza nummer: " + resultSet.getInt(+1)
-                        + ". Navnet på pizza: " + resultSet.getString("PizzaNavn")
-                        + ". Hvad der er på: " + resultSet.getString("Toppings")
-                        + ". Og hvad den koster: " + resultSet.getInt("Pris"));
+                System.out.println(
+                    "OrdreID: " + resultSet.getInt("ID") +", "+
+                    "PizzaNavn: " + resultSet.getString("PizzaNavn") +", "+
+                    "Toppings: " + resultSet.getString("Toppings") +", "+
+                    "Pris pr stk.: " + resultSet.getDouble("Pris") +", "+
+                    "Antal: " + resultSet.getInt("Antal") +", "+
+                    "Pris i alt: " + resultSet.getDouble("PrisTotal")
+                    );
+                totalprice += resultSet.getDouble("PrisTotal");
             }
+            System.out.println("Totalpris:"+ totalprice);
+
+
+
             connection.close();
+
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -41,5 +77,12 @@ public class Hpl {
 
     }
 
+    public static Connection connectDB() throws SQLException {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/pizza", "root", "");
+
+    }
+
+
 
 }
+
