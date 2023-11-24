8934733ea07f1ef9d8fab2fc2680051ffe45f7d3
GitMarkNielsen <Mark-Nielsen@outlook.com>
2023-11-24 17:39:05 +0100

    Revert "Fjernet et andet projekt, og opdateret Sabb's scener"
    This reverts commit 9fcc787855fcc5a093037907a12f13d21153ebcb.

--- sabbb/src/main/java/com/example/sabbb/HelloApplication.java
+++ sabbb/src/main/java/com/example/sabbb/HelloApplication.java
@@ -7,14 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
-public class Restaurant extends Application {
-
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Restaurant.class.getResource("your-order.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Login");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
